d5c9768b7a7c7077b307bd70f488608e18bfa9ff
Selena Zhou <selena.zhou@mongodb.com>
2025-07-28 17:43:48 -0400

    Set up bash logic for perf PR comment if is PR run

--- etc/perf-pr-comment.sh
+++ etc/perf-pr-comment.sh
@@ -4,6 +4,21 @@
 
 set -eux
 
+# Generate perf report.
 pushd ./internal/cmd/perfcomp >/dev/null || exist
-GOWORK=off go run main.go --project="mongo-go-driver" ${VERSION_ID}
+GOWORK=off go run main.go --project="mongo-go-driver" ${VERSION_ID} > perf-report.txt
 popd >/dev/null
+
+if [[ -n "${BASE_SHA+set}" && -n "${HEAD_SHA+set}" && "$BASE_SHA" != "$HEAD_SHA" ]]; then
+    # Make the PR comment.
+    echo "TODO: make the PR comment"
+    echo "Base SHA: ${BASE_SHA}"
+    echo "Head SHA: ${HEAD_SHA}"
+    # target=$DRIVERS_TOOLS/.evergreen/github_app/create_or_modify_comment.sh
+    # bash $target -c "$(pwd)/perf-report.md" -h $HEAD_SHA -o "mongodb" -n "mongo-go-driver"
+else
+    # Skip comment if it isn't a PR run.
+    echo "Skipping Perf PR comment"
+fi
+
+rm ./internal/cmd/perfcomp/perf-report.txt
